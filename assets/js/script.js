de12f7dabc97feca6bcccb5972256bd77b42dfb8
Ar1f007 <muhammadarif9416@yahoo.com>
2021-08-19 01:11:52 +0600

    trending section styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
+$(document).ready(function () {
+  const arrowLeft = `<button class="slide-arrow prev-arrow"><i class="fas fa-chevron-left"></i></button>`;
+  const arrowRight = `<button class="slide-arrow next-arrow active"><i class="fas fa-chevron-right"></i></button>`;
 
+  $(".slider").slick({
+    arrows: true,
+    speed: 1000,
+    slidesToShow: 3,
+    prevArrow: arrowLeft,
+    nextArrow: arrowRight,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
